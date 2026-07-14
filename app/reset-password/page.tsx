03d1fc86ff33af3c3c90a02785e347869ed5dfe2
Yashvi Ghetiya <patelyasvi495@gmail.com>
2026-07-14 19:01:42 +0530

    update reset-password page

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import { ResetPasswordForm } from "@/components/reset-password-form";
 
 export default function ResetPasswordPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
-        <ResetPasswordForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ResetPasswordForm />
+        </Suspense>
       </div>
     </div>
-  )
+  );
 }
